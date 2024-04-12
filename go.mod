8a20685ba2528502109092ce895cdd0ce263bd12
User Egor <egoeik112@gmail.com>
2024-04-12 13:21:45 +0300

    Build 1 stage api

---
+++ go.mod
@@ -0,0 +1,3 @@
+module D1-api
+
+go 1.22.2
---
+++ iternal/app/apiserver/apiserver.go
@@ -0,0 +1,12 @@
+package apiserver
+
+type APIserver struct {
+}
+
+func New() *APIserver {
+	return &APIserver{}
+}
+
+func (s *APIserver) Start() error {
+	return nil
+}
