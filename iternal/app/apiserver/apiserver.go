6e61b7d0f1079854c4f1366377b6e99bcf5fd6cc
User Egor <egoeik112@gmail.com>
2024-04-12 13:52:52 +0300

    Add logger

--- iternal/app/apiserver/apiserver.go
+++ iternal/app/apiserver/apiserver.go
@@ -1,15 +1,32 @@
 package apiserver
 
+import "github.com/sirupsen/logrus"
+
 type APIserver struct {
 	config *Config
+	logger *logrus.Logger
 }
 
 func New(config *Config) *APIserver {
 	return &APIserver{
 		config: config,
+		logger: logrus.New(),
 	}
 }
 
 func (s *APIserver) Start() error {
+	if err := s.configureLogger(); err != nil {
+		return err
+	}
+	s.logger.Info("starting api server")
+	return nil
+}
+
+func (s *APIserver) configureLogger() error {
+	level, err := logrus.ParseLevel(s.config.LogLevel)
+	if err != nil {
+		return err
+	}
+	s.logger.SetLevel(level)
 	return nil
 }
